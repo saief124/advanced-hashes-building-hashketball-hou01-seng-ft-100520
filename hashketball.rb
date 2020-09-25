33eb04e3f6e0077e28c09a0cd005d1c0401082fa
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-25 21:00:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,14 @@ require 'pry'
 def my_hash (key, value)
   {key=>value}
 end
+def game_hash
+  {}
+end
 test_hash=my_hash(:animal, "gorilla")
 color=""
 array_stat=[]
 attributes=[]
 stats={my_hash(:team_name, color),my_hash(:colors,array_stat),my_hash(:players, attributes)}
 Main_hash={my_hash(:home, stats), my_hash(:away, stats)}
-test_hash
-stats
-Main_hash
+game_hash
 binding.pry
