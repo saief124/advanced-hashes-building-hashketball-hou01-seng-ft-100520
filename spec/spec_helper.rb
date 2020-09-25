5df3b30d51b391fed86c2b6db057897d22409f0a
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-25 20:55:58 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,5 @@ require_relative '../hashketball'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # config here
-  def my_hash (key, value)
-    {key=>value}
-  end
-  test_hash=my_hash(:animal, "gorilla")
-  color=""
-  array_stat=[]
-  attributes=[]
-  stats={my_hash(:team_name, color),my_hash(:colors,array_stat),my_hash(:players, attributes)}
-  Main_hash={my_hash(:home, stats), my_hash(:away, stats)}
-  test_hash
-  stats
-  Main_hash
+
 end
