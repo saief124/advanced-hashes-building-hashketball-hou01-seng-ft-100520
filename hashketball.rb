683702ffbb5dff87cef5e6515910c5c30c8e18a0
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-25 20:23:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,11 +2,12 @@
 def my_hash (key, value)
   {key=>value}
 end
-str=""
-array=[]
+test_hash=my_hash(:animal, "gorilla")
+color=""
+array_stat=[]
 attributes=[]
-stats={my_hash(:team_name, str),my_hash(:colors,array),my_hash(:players, attributes)}
+stats={my_hash(:team_name, color),my_hash(:colors,array_stat),my_hash(:players, attributes)}
 Main_hash={my_hash(:home, stats), my_hash(:away, stats)}
-
+test_hash
 stats
 Main_hash
