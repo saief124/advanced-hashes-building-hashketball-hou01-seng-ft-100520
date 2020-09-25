a48d8cbe2857d7783c8312534659ee8d205aa878
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-25 23:28:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -35,5 +35,6 @@ home_stats=my_hash(:team_name, home_team_name).merge(my_hash(:colors, home_team_
 away_stats=my_hash(:team_name, away_team_name).merge(my_hash(:colors, away_team_color)).merge(my_hash(:players, away_attributes))
 game_hash=my_hash(:home, home_stats).merge(my_hash(:away, away_stats))
 game_hash
-binding.pry
+
 end
+binding.pry
