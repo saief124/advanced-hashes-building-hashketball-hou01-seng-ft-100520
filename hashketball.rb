c6b04cc2eba14d8a2ad2fbe46842bbef455fbf42
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-26 00:51:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,7 +12,7 @@ away_team_color=["Turquoise", "Purple"]
 
 home_attributes=
 [
-  
+
   [{:player_name=>"alan_anderson"}, {:number=>0}, {:shoe =>16}, {:points =>22}, {:rebounds =>12}, {:assists =>12}, {:steals =>3}, {:blocks =>1}, {:slam_dunks=>1}],
   [{:player_name=>"reggie_evans"}, {:number=>30}, {:shoe =>14}, {:points =>12}, {:rebounds =>12}, {:assists =>12}, {:steals =>12}, {:blocks =>12}, {:slam_dunks=>7}],
   [{:player_name=>"brook_lopez"}, {:number=>11}, {:shoe =>17}, {:points =>17}, {:rebounds =>19}, {:assists =>10}, {:steals =>3}, {:blocks =>1}, {:slam_dunks=>15}],
