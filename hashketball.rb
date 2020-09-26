50611fc15975d859868a3fc5a47b50e0d77912c8
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-26 01:01:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,8 +16,8 @@ home_attributes=
   [{:player_name=>"alan_anderson"}, {:number=>0}, {:shoe =>16}, {:points =>22}, {:rebounds =>12}, {:assists =>12}, {:steals =>3}, {:blocks =>1}, {:slam_dunks=>1}],
   [{:player_name=>"reggie_evans"}, {:number=>30}, {:shoe =>14}, {:points =>12}, {:rebounds =>12}, {:assists =>12}, {:steals =>12}, {:blocks =>12}, {:slam_dunks=>7}],
   [{:player_name=>"brook_lopez"}, {:number=>11}, {:shoe =>17}, {:points =>17}, {:rebounds =>19}, {:assists =>10}, {:steals =>3}, {:blocks =>1}, {:slam_dunks=>15}],
-  [{:player_name=>"mason_plumlee"}, {:number=>1}, {:shoe =>19}, {:points =>26}, {:rebounds =>11}, {:assists =>6}, {:steals =>3}, {:blocks =>8}, {:slam_dunks=>5]},
-  [{:player_name=>"jason_terry"}, {:number=>31}, {:shoe =>15}, {:points =>19}, {:rebounds =>2}, {:assists =>2}, {:steals =>4}, {:blocks =>11}, {:slam_dunks=>1]}
+  [{:player_name=>"mason_plumlee"}, {:number=>1}, {:shoe =>19}, {:points =>26}, {:rebounds =>11}, {:assists =>6}, {:steals =>3}, {:blocks =>8}, {:slam_dunks=>5}],
+  [{:player_name=>"jason_terry"}, {:number=>31}, {:shoe =>15}, {:points =>19}, {:rebounds =>2}, {:assists =>2}, {:steals =>4}, {:blocks =>11}, {:slam_dunks=>1}]
 ]
 
 away_attributes=
