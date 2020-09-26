f0f760286040b7a079f991b866726535e7ce9a02
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-26 00:58:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,11 +22,11 @@ home_attributes=
 
 away_attributes=
 [
-  [:player_name=>"jeff_adrien", :number=>4, :shoe =>18, :points =>10, :rebounds =>1, :assists =>1, :steals =>2, :blocks =>7, :slam_dunks=>2],
-  [:player_name=>"bismack_biyombo", :number=>0, :shoe =>16, :points =>12, :rebounds =>4, :assists =>7, :steals =>22, :blocks =>15, :slam_dunks=>10],
-  [:player_name=>"desagna_diop", :number=>2, :shoe =>14, :points =>24, :rebounds =>12, :assists =>12, :steals =>4, :blocks =>5, :slam_dunks=>5],
-  [:player_name=>"ben_gordon", :number=>8, :shoe =>15, :points =>33, :rebounds =>3, :assists =>2, :steals =>1, :blocks =>1, :slam_dunks=>0],
-  [:player_name=>"kemba_walker", :number=>33, :shoe =>15, :points =>6, :rebounds =>12, :assists =>12, :steals =>7, :blocks =>5, :slam_dunks=>12]
+  [{:player_name=>"jeff_adrien"}, {:number=>4}, {:shoe =>18}, {:points =>10}, {:rebounds =>1}, {:assists =>1}, {:steals =>2}, {:blocks =>7}, {:slam_dunks=>2}],
+  [{:player_name=>"bismack_biyombo"}, {:number=>0}, {:shoe =>16}, {:points =>12}, {:rebounds =>4}, {:assists =>7}, {:steals =>22}, {:blocks =>15}, {:slam_dunks=>10}],
+  [{:player_name=>"desagna_diop"}, {:number=>2}, {:shoe =>14}, {:points =>24}, {:rebounds =>12}, {:assists =>12}, {:steals =>4}, {:blocks =>5}, {:slam_dunks=>5}],
+  [{:player_name=>"ben_gordon"}, {:number=>8}, {:shoe =>15}, {:points =>33}, {:rebounds =>3}, {:assists =>2}, {:steals =>1}, {:blocks =>1}, {:slam_dunks=>0}],
+  [{:player_name=>"kemba_walker"}, {:number=>33}, {:shoe =>15}, {:points =>6}, {:rebounds =>12}, {:assists =>12}, {:steals =>7}, {:blocks =>5}, {:slam_dunks=>12}]
 ]
 
 
