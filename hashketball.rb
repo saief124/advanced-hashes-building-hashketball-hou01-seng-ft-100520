61eb12ea936f73b6c31e4165aecb85bfcd1eb8f4
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-26 02:12:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,14 +10,19 @@ home_team_color=["Black", "White"]
 away_team_name="Charlotte Hornets"
 away_team_color=["Turquoise", "Purple"]
 
+h_player1=["alan_anderson", 0, 16, 22, 12, 12, 3, 1, 1]
+h_player2=["reggie_evans", 30, 14, 12, 12, 12, 12, 12, 7]
+h_player3=["brook_lopez", 11, 17, 17, 19, 10, 3, 1, 15]
+h_player4=["mason_plumlee", 1, 19, 26, 11, 6, 3, 8, 5]
+h_player5=["jason_terry", 31, 15, 19, 2, 2, 4, 11, 1]
 home_attributes=
 [
 
-  [{:player_name=>"alan_anderson"}, {:number=> 0}, {:shoe=> 16}, {:points=> 22}, {:rebounds=> 12}, {:assists=> 12}, {:steals=> 3}, {:blocks=> 1}, {:slam_dunks=> 1}],
-  [{:player_name=>"reggie_evans"}, {:number=> 30}, {:shoe=> 14}, {:points=> 12}, {:rebounds=> 12}, {:assists=> 12}, {:steals=> 12}, {:blocks=> 12}, {:slam_dunks=> 7}],
-  [{:player_name=>"brook_lopez"}, {:number=>11}, {:shoe =>17}, {:points =>17}, {:rebounds =>19}, {:assists =>10}, {:steals =>3}, {:blocks =>1}, {:slam_dunks=>15}],
-  [{:player_name=>"mason_plumlee"}, {:number=>1}, {:shoe =>19}, {:points =>26}, {:rebounds =>11}, {:assists =>6}, {:steals =>3}, {:blocks =>8}, {:slam_dunks=>5}],
-  [{:player_name=>"jason_terry"}, {:number=>31}, {:shoe =>15}, {:points =>19}, {:rebounds =>2}, {:assists =>2}, {:steals =>4}, {:blocks =>11}, {:slam_dunks=>1}]
+  [{:player_name=> h_player1[0]}, {:number=> h_player1[1]}, {:shoe=> h_player1[2]}, {:points=> h_player1[3]}, {:rebounds=> h_player1[4]}, {:assists=> h_player1[5]}, {:steals=> h_player1[6]}, {:blocks=> h_player1[7]}, {:slam_dunks=> h_player1[8]}],
+  [{:player_name=> h_player2[0]}, {:number=> h_player2[1]}, {:shoe=> h_player2[2]}, {:points=> h_player2[3]}, {:rebounds=> h_player2[4]}, {:assists=> h_player2[5]}, {:steals=> h_player2[6]}, {:blocks=> h_player2[7]}, {:slam_dunks=> h_player2[8]}],
+  [{:player_name=> h_player3[0]}, {:number=> h_player3[1]}, {:shoe=> h_player3[2]}, {:points=> h_player3[3]}, {:rebounds=> h_player3[4]}, {:assists=> h_player3[5]}, {:steals=> h_player3[6]}, {:blocks=> h_player3[7]}, {:slam_dunks=> h_player3[8]}],
+  [{:player_name=> h_player4[0]}, {:number=> h_player4[1]}, {:shoe=> h_player4[2]}, {:points=> h_player4[3]}, {:rebounds=> h_player4[4]}, {:assists=> h_player4[5]}, {:steals=> h_player4[6]}, {:blocks=> h_player4[7]}, {:slam_dunks=> h_player4[8]}],
+  [{:player_name=> h_player5[0]}, {:number=> h_player5[1]}, {:shoe=> h_player5[2]}, {:points=> h_player5[3]}, {:rebounds=> h_player5[4]}, {:assists=> h_player5[5]}, {:steals=> h_player5[6]}, {:blocks=> h_player5[7]}, {:slam_dunks=> h_player5[8]}]
 ]
 
 away_attributes=
